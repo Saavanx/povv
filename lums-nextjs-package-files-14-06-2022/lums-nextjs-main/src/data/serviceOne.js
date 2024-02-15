50bf181757a72041cc979366283b884f988548fb
Sawan Singh <97459725+Saavanx@users.noreply.github.com>
2024-02-16 04:38:30 +0530

    done

---
+++ lums-nextjs-package-files-14-06-2022/lums-nextjs-main/src/data/serviceOne.js
@@ -0,0 +1,25 @@
+const serviceOne = {
+  title: "Lums can boost \n up your web traffic",
+  services: [
+    {
+      id: 1,
+      icon: "lums-icon-laptop",
+      title: "Speed \n Optimization",
+      text: "There are many variations of \n passages of lorem ipsum is \n simply free text but majority \n have suffered.",
+    },
+    {
+      id: 2,
+      icon: "lums-icon-presentation",
+      title: "Marketing \n Analysis",
+      text: "There are many variations of \n passages of lorem ipsum is \n simply free text but majority \n have suffered.",
+    },
+    {
+      id: 3,
+      icon: "lums-icon-target",
+      title: "SEO and \n Backlinks",
+      text: "There are many variations of \n passages of lorem ipsum is \n simply free text but majority \n have suffered.",
+    },
+  ],
+};
+
+export default serviceOne;
